a13b032e50220b9bc71875aacfcde77aed6794f6
mrossant <matteorossant@gmail.com>
2021-08-31 16:13:34 -0400

    Generating Piece

---
+++ chess/board.rb
@@ -0,0 +1,11 @@
+class Board
+
+    def initialize
+        @board = Array.new(8) { Array.new(8) }
+        @null_piece = NullPiece.new
+    end
+
+    private
+    attr_reader :null_piece
+end
+
---
+++ chess/piece.rb
@@ -0,0 +1,5 @@
+class Piece
+    def initialize
+        @piece = Piece.new
+    end
+end
